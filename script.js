30e7140e26cab435f693e2982f02450784dd758a
Brandon-Guerrero <brandon2001huesos@gmail.com>
2024-12-18 17:35:23 -0500

    agregando los array para la logica de mi pagina

---
+++ script.js
@@ -0,0 +1,67 @@
+const poemas = [
+  //1ro poema
+  {
+  titulo:"Letras Que Nacen De Ti",
+  contenido:"Algunos son solo poemas, otros saolo versos, pero todos te pertenecen. Son tuyos sin tus palabras y caricias, estas letras no exitirian."
+},
+//2do poema
+{
+  titulo:"El cambio del corazón",
+  contenido:"De reprocharle todo a la vida, a agradecerte por ponerte en la mia; de no encontarme conforme con nada, a desear que no te vayas; de correr cuando algo falla, a intentarlo una y otra vez para que no te vayas. Así es como un hombre cambia cuando encunetra a la mujer que ama."
+},
+//3ro poema
+{
+  titulo:"Luz encendida",
+  contenido:"Bajo el cielo estrellado, en tu mirada encuentro mi hogar, Tu esencia es el faro que guia mi camino sin cesar.Eres mas que un cuerpo, eres un alma radiente, Una dama real,autentica y elegante.No es falta de respeto, es admiracion sincera,Deseo profundo que en mi pecho reverbera.Quiero cuidarte como el más valioso tesoro,Y en cada gesto, domsotarte mi amor oro.Abrazarte con ternura, sentir tu piel cercana,Tocarte con pasión, fundirnos en una danza.Eres mi amada, mi compañera en esta vida, Y en cada momento eres mi luz encendida."
+},
+//4to poema
+{
+  titulo:" El sentido de amar",
+  contenido:" Te amo, pero ¿qué es amar?Es mirar a tus ojos y saber que en ellos encuentro mi hogar,besar tus labios y sentir que son mi dulce despertar,tomar tu mano y saber que juntos podemos volar.Es ver tu sonrisa y encontrar en ella mi felicidad,abrazarte y sentir que en tus brazos la tranquilidad.Por ti, amor, vale la pena luchar,porque contigo, mi vida cobra sentido, mi amor, mi eternidad."
+},
+//5to poema
+{
+  titulo:" Un futuro Soñado contigo",
+  contenido:" ¿Cómo expresar lo que anhelo contigo, mi amada? Sueño con ser un caballero, solicitar tu mano con la delicadeza de una rosa y unir nuestros destinos en el sagrado lazo del matrimonio. Quisiera ser un rebelde, pero no para causar estragos, sino para robar tu corazón sin dejar huella, conquistarte con el fuego de mi pasión. Imagino un futuro colmado de amor y risas, donde nuestros hijos sean la manifestación de nuestra unión, y juntos, como almas gemelas destinadas, cuidemos de ellos con ternura y dedicación. Visualizo un hogar donde solo reinen la lealtad y la fidelidad, donde tú y yo seamos los guardianes de nuestros fieles compañeros de cuatro patas, compartiendo cada latido de nuestro amor en la eternidad de su mirada"
+},
+//6to poema
+{
+  titulo:"Desnuda eres poesia ",
+  contenido:"En la intimidad, desnuda sobre mí,tu belleza resplandece, no tiene fin.Como la luna en su plenitud,tu piel desnuda es mi beatitud.En cada curva, un suspiro nace,en cada beso, el éxtasis se hace.Desnuda sobre mí, eres poesía,tu cuerpo es arte, pura armonía.En cada caricia, el mundo desvanece,y en tus ojos, el universo florece.Encima de mí, sin ropa, eres sublime,un sueño hecho realidad, un éxtasis sublime."
+},
+//7mo poema
+{
+  titulo:"El encanto de tu magia",
+  contenido:""
+},
+//8vo poema
+{
+  titulo:"",
+  contenido:""
+},
+//9no poema
+{
+  titulo:"",
+  contenido:""
+},
+//10mo poema
+{
+  titulo:"",
+  contenido:""
+},
+//11ro poema
+{
+  titulo:"",
+  contenido:""
+},
+//12do poema
+{
+  titulo:"",
+  contenido:""
+},
+//13ro poema
+{
+  titulo:"",
+  contenido:""
+},
+];
